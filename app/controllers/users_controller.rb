e1df8242f35f15fb44e274b00193ca24d91ade08
Stephan Pavlovic <stephan@railslove.com>
2023-01-02 18:25:37 +0100

    show all

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(slug: params[:id].downcase)
-    @transactions = @user.transactions.this_month.order(date: :desc, created_at: :desc).limit(5)
+    @transactions = @user.transactions.this_month.order(date: :desc, created_at: :desc)
     @balance = @user.transactions.this_month.balance
   end
 end
