782ddd8541a41bd14e65173e6102beac950bef71
Stephan Pavlovic <stephan@railslove.com>
2023-01-02 17:06:06 +0100

    switch order

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
   def show
     @group = Group.find_by(slug: params[:id].downcase)
-    @transactions = @group.transactions.this_month.order(date: :desc, created_at: :desc).limit(5)
+    @transactions = @group.transactions.this_month.order(date: :desc, created_at: :desc)
     @balance = @group.transactions.this_month.balance
   end
 end
