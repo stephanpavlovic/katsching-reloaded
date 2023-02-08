766255c77aaf412cefac62d01e3f6d8ce70694d0
Stephan Pavlovic <stephan@railslove.com>
2023-02-08 14:30:52 +0100

    remove preload

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
-pin "@hotwired/turbo", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "application"
+pin "@hotwired/turbo", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "turbo_power", to: "https://ga.jspm.io/npm:turbo_power@0.2.0/dist/index.js"
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
